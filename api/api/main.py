cada4dea4e26c1ee85175e186532eea6c5cd5a18
Tushar <tsbauskar0001@gmail.com>
2023-04-01 14:12:48 +0530

    Code optimization controller

--- api/api/main.py
+++ api/api/main.py
@@ -1,6 +1,6 @@
-from typing import Union
-
 from fastapi import FastAPI
+from controllers.optimize_code import optimize_code
+from models.optimize_code_input import OptimizeCodeInput
 
 app = FastAPI()
 
@@ -8,3 +8,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get('/optimize-code')
+def optimize_code_route(body: OptimizeCodeInput):
+    return {"ans": optimize_code(body)}
