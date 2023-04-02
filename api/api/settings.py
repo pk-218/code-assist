4a79275a562a453d4b93a71fb87e63f199f043fa
Pankaj Khushalani <2182001pk@gmail.com>
2023-04-02 17:27:26 +0530

    fix(deploy): env var
    Signed-off-by: Pankaj Khushalani <2182001pk@gmail.com>

--- api/api/settings.py
+++ api/api/settings.py
@@ -2,12 +2,10 @@ import os
 from pydantic import BaseSettings
 
 import openai
-import dotenv
-
 
 class Settings(BaseSettings):
     path: str = os.getcwd() + "/.env"
-    key: str = dotenv.get_key(path, "OPENAI_API_KEY")
+    key: str = os.getenv("OPENAI_API_KEY")
     model: str = "gpt-3.5-turbo"
     temperature: float = 0.4
     max_tokens: int = 250
