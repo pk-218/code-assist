be5e3beaf2eb85c343bc1d06403813e4b1417362
Pankaj Khushalani <2182001pk@gmail.com>
2023-04-02 21:14:39 +0530

    chore: publish extension
    Signed-off-by: Pankaj Khushalani <2182001pk@gmail.com>

--- codeassist/src/functions/writeUnitTest.ts
+++ codeassist/src/functions/writeUnitTest.ts
@@ -1,35 +1,38 @@
-import * as vscode from 'vscode';
-import { getSelectionRange } from '../utils';
-import axios from 'axios';
-import { BASE_URI } from '../constants';
+import * as vscode from "vscode";
+import { getSelectionRange } from "../utils";
+import axios from "axios";
+import { BASE_URI } from "../constants";
 
 const writeUnitTest = async (
-    context: vscode.ExtensionContext,
-    test_module: string
+  context: vscode.ExtensionContext,
+  test_module: string
 ) => {
-    const editor = vscode.window.activeTextEditor;
-    const selection = editor?.selection;
+  const editor = vscode.window.activeTextEditor;
+  const selection = editor?.selection;
 
-    if (selection && !selection.isEmpty) {
-        const selectionRange = getSelectionRange(selection);
-        const highlightedCode = editor.document.getText(selectionRange);
+  if (selection && !selection.isEmpty) {
+    const selectionRange = getSelectionRange(selection);
+    const highlightedCode = editor.document.getText(selectionRange);
 
-        console.log(highlightedCode);
+    console.log(highlightedCode);
 
-        try {
-            const { data } = await axios.post(`${BASE_URI}/ut`, {
-                code: highlightedCode,
-                language: editor.document.languageId,
-                test_module: test_module,
-            });
+    try {
+      const { data } = await axios.post(`${BASE_URI}/ut`, {
+        code: highlightedCode,
+        language: editor.document.languageId,
+        test_module: test_module,
+      });
 
-            editor.edit((editBuilder) => {
-                editBuilder.replace(selectionRange, data);
-            });
-        } catch (err) {
-            console.log(err);
-        }
+      editor.edit((editBuilder) => {
+        editBuilder.insert(
+          selection.end,
+          `\n\nimport ${test_module}\n\n` + data
+        );
+      });
+    } catch (err) {
+      console.log(err);
     }
+  }
 };
 
 export default writeUnitTest;
